b36033aa42bbf470a22e3302c4d26d59f1b82d8c
Django0033 <edpu0033@gmail.com>
2022-06-02 22:09:36 -0500

    #

--- w03/array-methods/arrays.js
+++ w03/array-methods/arrays.js
@@ -1,4 +1,4 @@
-let arr = ["one", "two", "three"];
+let arr = ["one", "two", "thjree"];
 let newArray = arr.map(htmlLi);
 console.log(newArray.join("\n"));
 let myList = document.getElementById("myList");
@@ -6,7 +6,7 @@ let myList = document.getElementById("myList");
 myList.innerHTML = newArray.join("\n");
 
 function htmlLi(stringItem) {
-    return "<li>" + stringItem + "</li>";
+  return "<li>" + stringItem + "</li>";
 }
 
 let letterGrades = ["A", "B", "A"];
@@ -15,13 +15,13 @@ let gpaPoints = letterGrades.map(getGpa);
 console.log(gpaPoints);
 
 function getGpa(letterGrade) {
-    let gpa = { A: 4, B: 3, C: 2, D: 1 };
+  let gpa = { A: 4, B: 3, C: 2, D: 1 };
 
-    return gpa[letterGrade];
+  return gpa[letterGrade];
 }
 
 function getGpaAverage(total, gpa) {
-    return total + gpa;
+  return total + gpa;
 }
 
 let gpaAverage = gpaPoints.reduce(getGpaAverage) / gpaPoints.length;
